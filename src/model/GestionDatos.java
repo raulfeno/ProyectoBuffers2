ecce5afec903d452dae9c6402bafd699a65c5b78
raulfeno <75309319+raulfeno@users.noreply.github.com>
2021-01-14 10:50:37 +0100

    Update GestionDatos.java

--- src/model/GestionDatos.java
+++ src/model/GestionDatos.java
@@ -2,21 +2,21 @@ package model;
 
 public class GestionDatos {
 
-	public GestionDatos() {
+	public GestionDatos() { Hola
 
 	}
 
-	//TODO: Implementa una funci髇 para abrir ficheros
+	//TODO: Implementa una funci贸n para abrir ficheros
 	
-	//TODO: Implementa una funci髇 para cerrar ficheros
+	//TODO: Implementa una funci贸n para cerrar ficheros
 	
 	public boolean compararContenido (String fichero1, String fichero2){
-		//TODO: Implementa la funci髇
+		//TODO: Implementa la funci贸n
 		return true;
 	}
 	
 	public int buscarPalabra (String fichero1, String palabra, boolean primera_aparicion){
-		//TODO: Implementa la funci髇
+		//TODO: Implementa la funci贸n
 		return 1;
 	}	
 
